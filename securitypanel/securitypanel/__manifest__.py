d8184e8d52088d4d2a98376218fe2cad76a2aafb
GonzaloUlecia <34105701+GonzaloUlecia@users.noreply.github.com>
2019-02-28 22:18:54 +0100

    Add files via upload

---
+++ securitypanel/securitypanel/__manifest__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Security Panel",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Este módulo te permite la administración de acciones tomadas por los responsables de la ciberseguridad respecto a los
+        equipos pertinentes.
+    """,
+
+    'author': "Gonzalo Ulecia",
+    'website': "",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+        'views/data.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
